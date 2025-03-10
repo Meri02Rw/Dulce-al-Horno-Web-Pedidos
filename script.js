d44346db1105573ab283a2d4bd5347ddfd68796b
meri ruiz <ruizmeri48@gmail.com>
2025-03-10 15:21:02 -0700

    index (en proceso)

---
+++ script.js
@@ -0,0 +1,7 @@
+function toggleMenu() {
+    const menu = document.querySelector('.menu');
+    const button = document.querySelector('.menu-toggle');
+
+    menu.classList.toggle('active');
+    button.classList.toggle('active');
+}

a6527ecd90a436e3da945f0aa790f98f9dc5e483
Meri02Rw <ruizmeri48@gmail.com>
2026-06-03 23:26:03 -0700

    Add files via upload

--- config/mail_config.php
+++ config/mail_config.php
@@ -1,7 +1,7 @@
 <?php
 define('MAIL_HOST', 'smtp.gmail.com');
-define('MAIL_USER', 'ruizmeri48@gmail.com'); // Cambia por tu correo
-define('MAIL_PASS', 'clffqahcnppatprj'); // Cambia por tu contraseña o app password
+define('MAIL_USER', 'test@gmail.com'); // Cambia por tu correo
+define('MAIL_PASS', '123456'); // Cambia por tu contraseña o app password
 define('MAIL_PORT', 587);
 define('MAIL_FROM_NAME', 'Dulce al Horno');
 ?>
